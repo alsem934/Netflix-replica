f22c58e26ef89d77492719d98364d45ea278d7dc
alsem934 <semanalamudin@gmail.com>
2024-12-14 22:59:04 -0800

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import "./component/Header/Header"
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <>
+
+     <Home/>
+
+
+    </>
+  )
+}
+
+export default App
